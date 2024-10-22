886d415cd7f9b392e76c2b026a5a65c45b693076
djembuka <djembuka@gmail.com>
2024-10-22 14:11:27 +0300

    Select component

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,29 @@ window.addEventListener('load', () => {
     const rightItem = switcher.querySelector(
       '.slr2-wizard-switcher-item--right'
     );
+    const leftCheckbox = document.getElementById('createSiteY');
+    const rightCheckbox = document.getElementById('createSiteN');
 
     leftBg.style.width = `${leftItem.clientWidth - 4}px`;
     rightBg.style.width = `${rightItem.clientWidth - 4}px`;
 
-    switcher.classList.add(
-      'slr2-wizard-switcher--loaded',
-      'slr2-wizard-switcher--left-acitve'
-    );
-    document
-      .getElementById('slr2WizardTab1')
-      .classList.add('slr2-wizard-tab--active');
+    if (leftCheckbox.getAttribute('checked') === 'checked') {
+      switcher.classList.add(
+        'slr2-wizard-switcher--loaded',
+        'slr2-wizard-switcher--left-acitve'
+      );
+      document
+        .getElementById('slr2WizardTab1')
+        .classList.add('slr2-wizard-tab--active');
+    } else if (rightCheckbox.getAttribute('checked') === 'checked') {
+      switcher.classList.add(
+        'slr2-wizard-switcher--loaded',
+        'slr2-wizard-switcher--right-acitve'
+      );
+      document
+        .getElementById('slr2WizardTab2')
+        .classList.add('slr2-wizard-tab--active');
+    }
 
     switcher.addEventListener('click', (e) => {
       if (e.target.classList.contains('slr2-wizard-switcher-item--left')) {
@@ -35,7 +47,8 @@ window.addEventListener('load', () => {
         document
           .getElementById('createSiteY')
           .setAttribute('checked', 'checked');
-        document.getElementById('createSiteN').removeAttribute('checked');
+        leftCheckbox.click();
+        rightCheckbox.removeAttribute('checked');
       } else {
         //buttons
         switcher.classList.remove('slr2-wizard-switcher--left-acitve');
@@ -51,7 +64,8 @@ window.addEventListener('load', () => {
         document
           .getElementById('createSiteN')
           .setAttribute('checked', 'checked');
-        document.getElementById('createSiteY').removeAttribute('checked');
+        rightCheckbox.click();
+        leftCheckbox.removeAttribute('checked');
       }
     });
   });
@@ -95,4 +109,79 @@ window.addEventListener('load', () => {
       }
     });
   });
+
+  //select
+  (() => {
+    const tab1 = document.getElementById('slr2WizardTab1');
+    if (!tab1) return;
+    const select = tab1.querySelector('select');
+    const component = tab1.querySelector('.twpx-form-control--select');
+    if (!select || !component) return;
+    const content = component.querySelector(
+      '.twpx-form-control-select__content'
+    );
+    const control = component.querySelector('.twpx-form-control-select');
+    const hidden = component.querySelector('input[type="hidden"]');
+
+    //create options
+    select.querySelectorAll('option').forEach((o) => {
+      const option = document.createElement('div');
+      option.classList.add('twpx-form-control-select__dropdown-item');
+      option.setAttribute('data-value', o.getAttribute('value'));
+      option.innerText = o.innerText;
+
+      option.addEventListener('click', (e) => {
+        e.preventDefault();
+        clickItem(option);
+      });
+
+      document
+        .querySelectorAll('#slr2WizardTab1 .twpx-form-control-select__dropdown')
+        .forEach((dd) => {
+          dd.append(option);
+        });
+    });
+
+    //content
+    content.addEventListener('click', openHideDropdown);
+
+    //document click
+    document.addEventListener('click', (e) => {
+      if (
+        e.target.className !== 'twpx-form-control-select__dropdown-item' &&
+        e.target.className !== 'twpx-form-control-select__content'
+      ) {
+        hideDropdown();
+      }
+    });
+
+    function openHideDropdown(e) {
+      e.preventDefault();
+      control.classList.toggle('twpx-form-control-select--dropdown');
+    }
+
+    function hideDropdown() {
+      control.classList.remove('twpx-form-control-select--dropdown');
+    }
+
+    function clickItem(option) {
+      const value = option.getAttribute('data-value');
+
+      content.innerText = option.innerText;
+      hidden.value = value;
+
+      hideDropdown();
+
+      component.classList.add('twpx-form-control-select--selected');
+
+      //onchange
+      if (typeof customOnChange === 'function') {
+        customOnChange(value);
+      }
+    }
+
+    function customOnChange(value) {
+      tab1.querySelector('[name="__wiz_siteID"]').value = value;
+    }
+  })();
 });
